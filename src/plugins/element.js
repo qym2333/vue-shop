ce44ad84d56b6b805ae0d93f4c31f7ee2f4068dc
even <731788284@qq.com>
2021-01-25 20:24:22 +0800

    首页基本布局

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,7 +5,14 @@ import {
   FormItem,
   Input,
   Message,
-  Loading
+  Loading,
+  Container,
+  Header,
+  Aside,
+  Main,
+  Menu,
+  Submenu,
+  MenuItem
 } from 'element-ui'
 
 Vue.use(Button)
@@ -13,4 +20,13 @@ Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
 Vue.use(Loading)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+
+// 全局消息提示
 Vue.prototype.$message = Message
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,31 @@
 <template>
-  <div>
-    <el-button plain @click="onLogout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <div class="left">
+        <img src="../assets/login-0.png" alt="">
+        <span>后台管理系统</span>
+      </div>
+      <el-button plain @click="onLogout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside width="200px">
+        <el-menu default-active="2">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -24,7 +48,39 @@ export default {
     }
   }
 }
+
 </script>
 
 <style scoped lang='less'>
+.home-container {
+  height: 100%;
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    // background-color: #0e8bff;
+    border-bottom: 2px solid #0e8bff;
+    .left {
+      display: flex;
+      align-items: center;
+      span {
+        font-size: 24px;
+        color: #0e8bff;
+        margin-left: 10px;
+      }
+      img {
+        width: 250/4px;
+        height: 198/4px;
+      }
+    }
+  }
+  .el-container {
+    .el-aside {
+      // background-color: #0e8bff;
+      .el-menu {
+        height: 100%;
+      }
+    }
+  }
+}
 </style>
