abc16eedb485a5272d15991d4d828e1b020cfc88
even <731788284@qq.com>
2021-01-25 18:32:53 +0800

    完成登录页面

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <el-button plain @click="onLogout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+
+  },
+
+  methods: {
+    onLogout () {
+      sessionStorage.removeItem('token')
+      this.$router.push('/login')
+      this.$message.success('账号退出……')
+    }
+  }
+}
+</script>
+
+<style scoped lang='less'>
+</style>
